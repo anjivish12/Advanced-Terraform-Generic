c7a0897747279e92e26fd07f79b78cbca4d21d3f
anjali <anjali@gmail.com>
2025-11-09 21:59:08 +0530

    dev folder added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "../modules/azurerm_rg"
+  source = "../../modules/azurerm_rg"
   rgs    = var.rgs
 }
 
@@ -10,30 +10,30 @@ module "virtual_network" {
 }
 module "public_ip" {
   depends_on = [module.resource_group]
-  source     = "../modules/azurerm_public_ip"
+  source     = "../../modules/azurerm_public_ip"
   pips       = var.pips
 }
 
 module "kv" {
   depends_on = [module.resource_group]
-  source     = "../modules/azurerm_key_vault"
+  source     = "../../modules/azurerm_key_vault"
   keys       = var.keys
 }
 
 module "secret" {
   depends_on = [module.kv]
-  source     = "../modules/azurerm_kv_secret"
+  source     = "../../modules/azurerm_kv_secret"
   secrets    = var.secrets
 }
 
 module "nsg" {
   depends_on = [ module.resource_group, module.virtual_network ]
-  source = "../modules/azurerm_nsg"
+  source = "../../modules/azurerm_nsg"
   nsgs = var.nsgs
   
 }
 module "vm" {
   depends_on = [module.resource_group, module.public_ip, module.virtual_network, module.kv, module.secret]
-  source     = "../modules/azurerm_vm"
+  source     = "../../modules/azurerm_vm"
   vms        = var.vms
 }
